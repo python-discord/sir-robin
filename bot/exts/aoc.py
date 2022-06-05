e8dbfb1810369ad01e9a4b6874619b436a93f866
Robin5605 <robinjefferson123@gmail.com>
2022-06-04 22:32:41 -0500

    Lint

--- bot/exts/aoc.py
+++ bot/exts/aoc.py
@@ -1,12 +1,14 @@
 from typing import Optional
+
 import discord
 from discord.ext import commands, tasks
 from discord.utils import MISSING
 
-from bot.bot import SirRobin
 from bot import constants
+from bot.bot import SirRobin
 
 aoc_URL = "https://adventofcode.com/{year}/day/{day}"
+
 
 class OffSeasonAoC(commands.Cog):
     def __init__(self, bot: SirRobin) -> None:
@@ -20,18 +22,18 @@ class OffSeasonAoC(commands.Cog):
         if self.current_day > 25:
             self.loop.cancel()
             return
-        
+
         channel: discord.ForumChannel = self.bot.get_channel(constants.Channels.aoc_forum_channel)
         link = aoc_URL.format(year=self.year, day=self.current_day)
         await channel.create_thread(name=f"Off-season AoC #{self.current_day}", content=link)
-        
+
         self.current_day += 1
 
     @commands.command()
     async def summer_aoc(self, ctx: commands.Context, year: int, days: int, start_day: int = 1) -> None:
         if not (year >= 2015 and year <= 2021):
             raise commands.BadArgument("Year must be between 2015 and 2021, inclusive")
-        
+
         if not (start_day >= 1 and start_day <= 25):
             raise commands.BadArgument("Start day must be between 1 and 25, inclusive")
 
@@ -43,12 +45,12 @@ class OffSeasonAoC(commands.Cog):
         else:
             await ctx.send("Starting loop...")
             self.loop = tasks.Loop(
-                self.aoc_task, 
-                seconds=0, 
-                minutes=0, 
-                hours=(days * 24), 
-                time=MISSING, 
-                count=None, 
+                self.aoc_task,
+                seconds=0,
+                minutes=0,
+                hours=(days * 24),
+                time=MISSING,
+                count=None,
                 reconnect=True
             )
 
@@ -57,11 +59,12 @@ class OffSeasonAoC(commands.Cog):
     @commands.command()
     async def stop_aoc(self, ctx: commands.Context) -> None:
         if self.loop:
-            self.loop.cancel() # .cancel() doesn't allow the current iteration to finish
+            self.loop.cancel()  # .cancel() doesn't allow the current iteration to finish
             await ctx.send("Stopped running AoC loop")
         else:
             await ctx.send("The AoC loop is currently not running.")
-        
+
+
 async def setup(bot: SirRobin):
     """Load the summer AoC cog"""
     await bot.add_cog(OffSeasonAoC(bot))
