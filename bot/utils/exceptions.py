80a78d13a8aaab0297f033b4f2e83b3743342216
Brody Critchlow <24994624+brodycritchlow@users.noreply.github.com>
2023-11-28 16:02:21 -0700

    Removed all references to `!subscribe` (#100)

--- bot/utils/exceptions.py
+++ bot/utils/exceptions.py
@@ -8,11 +8,3 @@ class JamCategoryNameConflictError(Exception):
 
 class CodeJamCategoryCheckFailure(CheckFailure):
     """Raised when the specified command was run outside the Code Jam categories."""
-
-
-
-class MovedCommandError(Exception):
-    """Raised when a command has moved locations."""
-
-    def __init__(self, new_command_name: str):
-        self.new_command_name = new_command_name
